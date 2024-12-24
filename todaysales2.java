57da6a52246f2466681e3b5f4b7d1cc80537c698
Nithinsiliverii <mr.carskingnithin123@gmail.com>
2024-12-24 15:38:47 +0530

    first commit

---
+++ todaysales2.java
@@ -0,0 +1,38 @@
+package sales;
+import java.util.Scanner;
+public class todaysales2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter no of rows");
+		int r = sc.nextInt();
+		System.out.println("enter no of columns");
+		int c = sc.nextInt();
+		
+		
+		int[][] matrix = new int[r][c];
+		
+		for(int rows=0;rows<r;rows++) {
+			for(int cols=0;cols<c;cols++) {
+				
+				matrix[rows][cols]= sc.nextInt();
+			}
+			
+		}		for(int rows=0;rows<r;rows++) {
+			for(int cols=0;cols<c;cols++) {
+				
+				System.out.print(matrix[rows][cols]+" ");
+			
+			}
+			System.out.println();
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+	}
+
